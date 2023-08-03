7d13eb660189f52878b40c48cd994a27ceae8b5d
blinry <mail@blinry.org>
2023-08-03 14:52:06 +0200

    Add space for cables under the motor

--- clip.py
+++ clip.py
@@ -4,6 +4,7 @@ motor_diameter = 10 + 0.3 # mm
 motor_height = 3 + 0.3
 thickness = 1
 thickness_bottom = 0.6
+cable_thickness = 1.5
 
 belt_hole_distance = 17.5
 belt_hole_diameter = 5
@@ -16,7 +17,7 @@ height = motor_diameter + thickness
 result = (
     cq.Workplane("front")
     .rect(width/2, height)
-    .extrude(motor_height + thickness_bottom)
+    .extrude(motor_height + thickness_bottom + cable_thickness)
     
     .faces(">Z")
     .workplane()
@@ -28,7 +29,7 @@ result = (
     .workplane()
     .moveTo(-width/8)
     .circle(belt_hole_diameter/2)
-    .extrude(clip_protrusion/2, taper=-45)
+    .extrude(clip_protrusion*0.75, taper=-45)
         
     # .faces(">Z")
     # .workplane()
@@ -39,13 +40,13 @@ result = (
     .faces(">Z")
     .workplane()
     .moveTo(-width/8)
-    .circle(belt_hole_diameter/2*1.15)
+    .circle(belt_hole_diameter*0.61)
     .extrude(clip_protrusion, taper=45)
     
     .faces(">Z")
     .workplane()
     .moveTo(-width/8)
-    .rect(2,belt_hole_diameter*1.2)
+    .rect(2,belt_hole_diameter*1.3)
     .cutThruAll()
     )
 
@@ -54,12 +55,33 @@ result = (
     .faces(">Z")
     .workplane()
     .moveTo(width/4, 0)
-    .hole(motor_diameter, motor_height + belt_hole_height + clip_protrusion)
-    .faces(">Y")
-    .workplane()
-    .moveTo(-width/4,-height*0.45)
-    .hole(6, thickness*2)
+    .hole(motor_diameter, motor_height + belt_hole_height + clip_protrusion + cable_thickness + 0.25)
+    # .faces(">Y")
+    # .workplane()
+    # .moveTo(-width/4,-height*0.45)
+    # .hole(8, thickness*2)
 )
+
+ridge = (
+    cq.Workplane("XY")
+    .moveTo(width/4,0)
+    .rect(1,height)
+    .extrude(cable_thickness + thickness_bottom)
+)
+
+hole = (
+    cq.Workplane("XY")
+    .workplane(thickness_bottom)
+    .moveTo(width/4,height/2)
+    .rect(4,motor_diameter)
+    .extrude(cable_thickness*6)
+)
+    
+
+result = result.cut(hole)
+result = result.union(ridge)
+
+
 
 
 # result = (
